756bf67d889e62ee838d8e32369ef37bae0b6f17
deb-alien <DevPro-2011@outlook.com>
2026-05-27 17:11:18 +0600

    fix(js-dependency-update): remove unused @octokit/request-error dependency and clean up comments

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -31,24 +31,6 @@ const setupLogger = ({ debug, prefix } = { debug: false, prefix: '' }) => ({
 	},
 });
 
-/**
- * [DONE]
- * 1. Phase inputs
- * 	- base-branch from which to check for updates
- *  - target-branch to use to create the PR
- *  - Github token to authentication purposes (to create the PRs)
- *  - Working directory for which to check for dependencies
- * [DONE]
- * 2. Execute npm update command within the working directory
- * [DONE]
- * 3. Check whether are modified package*.json files
- * [TODO]
- * 4.  If there are modified files:
- * 	- Add and commit to the target branch
- *  - create a PR to the base branch using the octokit api
- * 5. Otherwise conclude the custom action
- */
-
 async function run() {
 	try {
 		info('Hello from the JS Dependency Update Action!');
@@ -103,7 +85,7 @@ async function run() {
 		}
 
 		updatesAvailable = true;
-		
+
 		logger.debug('Updates Available');
 		logger.debug('Setting up Git');
 
@@ -143,7 +125,7 @@ async function run() {
 		logger.info('Pull request created successfully. Concluding action execution.');
 
 		logger.debug(`Setting updates-available output to ${updatesAvailable}`);
-		setOutput('updates-available', updatesAvailable);
+		setOutput('update_available', updatesAvailable);
 
 		return; //* Conclude the action execution after creating the PR
 	} catch (error) {
